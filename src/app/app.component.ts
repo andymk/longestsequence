19442ca5d3e402e62b530c085a8ee74e1a7b091d
Andy Knight <andym.knight@gmail.com>
2019-01-19 17:23:40 +0000

    added results table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LongestSequenceService } from 'src/services/longest-sequence.service';
+import { IResultRow } from 'src/model/IResultRow';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,17 @@ import { LongestSequenceService } from 'src/services/longest-sequence.service';
 export class AppComponent {
   string1: string = "";
   string2: string = "";
+  results: IResultRow[] = [];
+
 
   constructor(private longestSequenceService: LongestSequenceService) {}
 
   search() {
     let result = this.longestSequenceService.getSequence(this.string1, this.string2);
-    console.log(result);
+    let r = {} as IResultRow;
+    r.string1 = this.string1;
+    r.string2 = this.string2;
+    r.result = result;
+    this.results.push(r);
   }
 }
---
+++ src/model/IResultRow.ts
@@ -0,0 +1,5 @@
+export interface IResultRow {
+    string1: string;
+    string2: string;
+    result: string;
+}
